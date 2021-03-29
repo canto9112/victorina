76dc7dad113788971a78f3a0627625e217b22ceb
canto9112 <cantoweb112@gmail.com>
2021-03-29 21:58:05 +0300

    Новая функция открытия файлов викторины

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from environs import Env
 
 
+def open_file(file_path):
+    with open(file_path, 'r', encoding='KOI8-R') as file:
+        content = file.read()
+    return content
+
+
 def main():
     pass
 
@@ -8,5 +14,6 @@ def main():
 if __name__ == '__main__':
     env = Env()
     env.read_env()
-    
+
     main()
+    print(open_file('quiz-questions/1vs1200.txt'))
