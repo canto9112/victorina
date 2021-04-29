5151a966a462f11ec4922a3ef9eeeea0828ced33
canto9112 <cantoweb112@gmail.com>
2021-04-29 18:42:42 +0300

    Настроил парсинг всех файлов

--- questions.py
+++ questions.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 
 def open_file(file_path):
@@ -7,8 +8,9 @@ def open_file(file_path):
     return content
 
 
-def get_questions():
-    file = open_file('quiz-questions/9krug16.txt')
+def get_questions(filename):
+    file = open_file(f'quiz-questions/{filename}')
+
     content = file.split('\n\n')
     block_questions = []
     block_answers = []
@@ -20,15 +22,17 @@ def get_questions():
 
             block_answers.append(block)
     questions = dict(zip(block_questions, block_answers))
-
     return questions
 
 
 def get_random_question():
-    questions = get_questions()
-    random_question, random_answer = random.choice(list(questions.items()))
+    directory = os.listdir('quiz-questions')
+    for file in directory:
+        questions = get_questions(file)
+        random_question, random_answer = random.choice(list(questions.items()))
+        question = ",".join(random_question.split('\n')[1:])
+        answer = ",".join(random_answer.split('\n')[1:])
+        clean_answer = answer.split('.')[0]
 
-    question = ",".join(random_question.split('\n')[1:])
-    answer = ",".join(random_answer.split('\n')[1:])
-    clean_answer = answer.split('.')[0]
-    return question, clean_answer
+        return question, clean_answer
+
